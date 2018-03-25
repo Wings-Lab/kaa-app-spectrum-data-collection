20e37e1d7528745cc0f6fc75b69a9b4199e1bb64
Arian Niaki <arianniaki@gmail.com>
2018-03-25 18:26:34 -0400

    adding source files from snigda

---
+++ build/kaa/CMakeFiles/kaacpp.dir/DependInfo.cmake
@@ -0,0 +1,94 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/ClientStatus.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/ClientStatus.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/Kaa.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/Kaa.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/KaaClient.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/KaaClient.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/KaaClientProperties.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/KaaClientProperties.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/KaaDefaults.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/KaaDefaults.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/bootstrap/BootstrapManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/bootstrap/BootstrapManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/bootstrap/BootstrapTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/bootstrap/BootstrapTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/IPTransportInfo.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/IPTransportInfo.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/KaaChannelManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/KaaChannelManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/RedirectionTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/RedirectionTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/SyncDataProcessor.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/SyncDataProcessor.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/TransportProtocolIdConstants.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/TransportProtocolIdConstants.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/connectivity/IPConnectivityChecker.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/connectivity/IPConnectivityChecker.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/connectivity/PingConnectivityChecker.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/connectivity/PingConnectivityChecker.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/impl/AbstractHttpChannel.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/impl/AbstractHttpChannel.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/impl/DefaultBootstrapChannel.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/impl/DefaultBootstrapChannel.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/impl/DefaultOperationHttpChannel.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/impl/DefaultOperationHttpChannel.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/impl/DefaultOperationLongPollChannel.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/impl/DefaultOperationLongPollChannel.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/channel/impl/DefaultOperationTcpChannel.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/channel/impl/DefaultOperationTcpChannel.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/common/EndpointObjectHash.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/common/EndpointObjectHash.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/configuration/ConfigurationTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/configuration/ConfigurationTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/configuration/manager/ConfigurationManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/configuration/manager/ConfigurationManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/configuration/storage/FileConfigurationStorage.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/configuration/storage/FileConfigurationStorage.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/context/AbstractExecutorContext.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/context/AbstractExecutorContext.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/context/SimpleExecutorContext.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/context/SimpleExecutorContext.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/event/EventManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/event/EventManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/event/EventTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/event/EventTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/event/registration/EndpointRegistrationManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/event/registration/EndpointRegistrationManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/event/registration/UserTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/event/registration/UserTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/failover/DefaultFailoverStrategy.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/failover/DefaultFailoverStrategy.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/http/HttpClient.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/http/HttpClient.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/http/HttpResponse.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/http/HttpResponse.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/http/HttpUrl.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/http/HttpUrl.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/http/MultipartPostHttpRequest.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/http/MultipartPostHttpRequest.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/kaatcp/ConnackMessage.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/kaatcp/ConnackMessage.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/kaatcp/KaaSyncResponse.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/kaatcp/KaaSyncResponse.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/kaatcp/KaaTcpCommon.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/kaatcp/KaaTcpCommon.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/kaatcp/KaaTcpParser.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/kaatcp/KaaTcpParser.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/kaatcp/KaaTcpResponseProcessor.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/kaatcp/KaaTcpResponseProcessor.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/DefaultLogUploadStrategy.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/DefaultLogUploadStrategy.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/LogCollector.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/LogCollector.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/LogStorageConstants.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/LogStorageConstants.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/LoggingTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/LoggingTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/MemoryLogStorage.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/MemoryLogStorage.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/log/RecordFuture.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/log/RecordFuture.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/logging/DefaultLogger.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/logging/DefaultLogger.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/logging/Log.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/logging/Log.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/logging/LoggingUtils.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/logging/LoggingUtils.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/notification/NotificationManager.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/notification/NotificationManager.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/notification/NotificationTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/notification/NotificationTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/profile/ProfileTransport.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/profile/ProfileTransport.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/security/KeyUtils.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/security/KeyUtils.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/security/RsaEncoderDecoder.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/security/RsaEncoderDecoder.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/transport/HttpDataProcessor.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/transport/HttpDataProcessor.cpp.o"
+  "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/kaa/impl/utils/ThreadPool.cpp" "/home/arian/Documents/Kaa_Applications/SpecSense/SDK2/build/kaa/CMakeFiles/kaacpp.dir/impl/utils/ThreadPool.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_LOG_DYN_LINK"
+  "KAA_DEFAULT_BOOTSTRAP_HTTP_CHANNEL"
+  "KAA_DEFAULT_CONNECTIVITY_CHECKER"
+  "KAA_DEFAULT_LONG_POLL_CHANNEL"
+  "KAA_DEFAULT_OPERATION_HTTP_CHANNEL"
+  "KAA_DEFAULT_TCP_CHANNEL"
+  "KAA_MAX_LOG_LEVEL=3"
+  "KAA_RUNTIME_KEY_GENERATION"
+  "KAA_THREADSAFE"
+  "KAA_USE_CONFIGURATION"
+  "KAA_USE_EVENTS"
+  "KAA_USE_LOGGING"
+  "KAA_USE_NOTIFICATIONS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../kaa"
+  "/usr/local/include"
+  "/usr/local/include/botan-1.11"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
